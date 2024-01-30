fe8e6ee6a11209b3633dcf91424dfa1ff7a4d8d6
Technoblade <william@williamgiles.co.nz>
2024-01-31 09:02:55 +1300

    fix battery screen

--- lcd/src/screens/Settings/batteryScreen.h
+++ lcd/src/screens/Settings/batteryScreen.h
@@ -22,13 +22,20 @@ void batteryScreen::draw()
   display.sprite.setTextColor(TFT_CYAN);
   display.sprite.drawString("Battery", 10, 30);
 
+  int percent = battery.getPercentageI();
+  percent = constrain(percent, 0, 100);
+  int height = map(percent, 0, 100, 0, 121);
+
+  u16_t color = TFT_GREEN;
+  if (percent < 20)
+    color = TFT_RED;
+  else if (percent < 50)
+    color = TFT_ORANGE;
+
   display.sprite.drawRoundRect((LCD_WIDTH / 4 * 3) - 25, 36, 50, 125, 5, TFT_WHITE);
   display.sprite.fillRect((LCD_WIDTH / 4 * 3) - 5, 29, 10, 7, TFT_WHITE);
 
-  int percent = battery.getPercentageI();
-  int height = map(percent, 0, 100, 0, 121);
-
-  display.sprite.fillRoundRect((LCD_WIDTH / 4 * 3) - 23, 36 + 2 + (121 - height), 46, height, 5, TFT_WHITE);
+  display.sprite.fillRoundRect((LCD_WIDTH / 4 * 3) - 23, 36 + 2 + (121 - height), 46, height, 5, color);
 
   display.sprite.setTextSize(4);
   display.sprite.setTextColor(TFT_WHITE);
